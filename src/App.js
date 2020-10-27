e7af25a9b1e8ad1671e7047571f3d059c468512b
GoglichidzeMike <goglichidze.mike@gmail.com>
2020-10-28 02:48:08 +0400

    completed logic and a little design

--- src/App.js
+++ src/App.js
@@ -11,34 +11,64 @@ import { bankTwo } from "./components/banks";
 
 function App() {
   const [currentPad, setCurrentPad] = useState(bankOne);
-  const [power, setPower] = useState("true");
+  const [power, setPower] = useState(false);
   const [volume, setVolume] = useState(0.3);
   const [display, setDisplay] = useState("Power Off");
 
+  //power switch + display set
   const handlePadChange = () => {
-    setCurrentPad(currentPad === bankOne ? bankTwo : bankOne);
+    if (power) {
+      if (currentPad === bankOne) {
+        setCurrentPad(bankTwo);
+        setDisplay("Smooth Piano Kit");
+      } else {
+        setCurrentPad(bankOne);
+        setDisplay("Heater Kit");
+      }
+    }
   };
 
   const handlePowerChange = () => {
+    //changes power state
     setPower(!power);
+    //changes display text as per power
     setDisplay(display === "Power Off" ? "Power On" : "Power Off");
+
+    //clears display in 1 second
+    //in future we want to change this to green button
+    setTimeout(() => clearDisplay(), 1000);
   };
 
   const handleVolumeChange = (e) => {
     if (power) {
+      //set state to new value
       setVolume(e.target.value);
+      // set display to new value
       setDisplay("Volume " + Math.round(e.target.value * 100));
     }
+    //clear display after 1 second.
     setTimeout(() => clearDisplay(), 1000);
   };
 
+  //clears display
   const clearDisplay = () => {
-    setDisplay("Power On");
+    setDisplay("");
+  };
+
+  //update display via pad name
+  const updateDisplay = (name) => {
+    if (power) {
+      setDisplay(name);
+    }
   };
 
   return (
     <div className="inner-container" id="drum-machine">
-      <PadBank currentPad={currentPad} power={power} />
+      <PadBank
+        currentPad={currentPad}
+        power={power}
+        updateDisplay={updateDisplay}
+      />
       <Controls
         adjustVolume={handleVolumeChange}
         volume={volume}
